88208a27f0ee636b6f880465f3d7e6b717b3d0d9
liufulong <qq65long@126.com>
2020-03-20 21:51:10 +0800

    commit

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: 'syslong',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false,
+  // 开启tagsView 页签模式
+  tagsView: true
+}
