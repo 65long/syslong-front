87d0cfe0976827673dfe599e612ed2c548509345
liufulong <qq65long@126.com>
2020-09-13 21:35:19 +0800

    [imp]添加echart图表

--- src/main.js
+++ src/main.js
@@ -17,6 +17,9 @@ import '@/permission' // permission control
 
 import i18n from './lang' // 国际化
 
+import echarts from "echarts";
+Vue.prototype.$echarts = echarts;
+
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,30 +1,63 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
+    <div id="main" style="width: 600px;height:400px;"></div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+  import {mapGetters} from 'vuex'
 
-export default {
-  name: 'Dashboard',
-  computed: {
-    ...mapGetters([
-      'name'
-    ])
+  export default {
+    name: 'Dashboard',
+    computed: {
+      ...mapGetters([
+        'name'
+      ])
+    },
+    mounted() {
+      this.drawChart();
+    },
+    methods: {
+      drawChart() {
+        // 基于准备好的dom，初始化echarts实例
+        let myChart = this.$echarts.init(document.getElementById("main"));
+        // 指定图表的配置项和数据
+        let option = {
+          title: {
+            text: "ECharts 入门示例"
+          },
+          tooltip: {},
+          legend: {
+            data: ["销量"]
+          },
+          xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+          },
+          yAxis: {},
+          series: [
+            {
+              name: "销量",
+              type: "bar",
+              data: [-1, 20, 36, 10, 10, 20]
+            }
+          ]
+        };
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+      }
+    }
   }
-}
 </script>
 
 <style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
+  .dashboard {
+    &-container {
+      margin: 30px;
+    }
+    &-text {
+      font-size: 30px;
+      line-height: 46px;
+    }
   }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
 </style>
